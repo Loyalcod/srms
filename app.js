a58e852e571d7eee8e9ff0ff77612073cb54aa7d
akujuobi victor <victorakujuobichibuike@gmail.com>
2022-09-24 05:38:48 +0100

    this is changes made

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const path = require("path")
+
+
+const port = process.env.port
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.use(cookieParser)
+
+app.get('/',(req,res)=>{
+    res.send("this is the server side")
+})
+
+
+app.listen(port,()=>{
+    console.log("this server is running")
+})

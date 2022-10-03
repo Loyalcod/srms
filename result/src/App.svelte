f70d468532dee0defa17d151bdeea8b5b9fca167
akujuobi victor <victorakujuobichibuike@gmail.com>
2022-10-03 22:10:21 +0100

    learning how to work with my sveltejs

--- result/src/App.svelte
+++ result/src/App.svelte
@@ -1,30 +1,44 @@
 <script>
-	export let name;
+	let name = 'hello guys welcome to the student result management system'
+	import Outer from './Outer.svelte'
+	let count = 0
+	const handleClick = () =>{
+		count += 1
+	}
+	const num = 10
+
+	let loggedin = false
+	const toggle = ()=>{
+
+		loggedin = !loggedin
+	}
+	
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>{name}</h1>
+	<Outer word =  'component is svelte'/>
+	<Outer />
+	<button on:click= {handleClick}> click {count} times</button>
+	{#if loggedin}
+	<button on:click={toggle}>Logout</button>
+	{:else}
+	<button on:click={toggle}>Login</button>
+	{/if}
+
+	{#if num > 10}
+	 	<p> {num} is greater than 10 </p>
+	{:else if 5 < num}
+		<p> 5 is less than {num} </p>
+	{:else}
+	 	{num} is between 5 and 10
+	{/if}
+
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	h1{
+		color: red;
+		font-size: 20px;
 	}
 </style>
---
+++ result/src/Outer.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let word = 'default value'
+</script>
+
+
+<main>
+    <p>{word}</p>
+</main>
